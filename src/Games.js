2a84949ff276688ba38b417b1ae7a068dc5aae13
Cardinale <Csar>
2020-03-22 03:50:03 +0100

    Début du plateau de la contrée : attente des joueurs (2) fix fix

--- src/Games.js
+++ src/Games.js
@@ -1,5 +1,5 @@
 import openSocket from 'socket.io-client';
-const socket = openSocket('http://localhost:8000');
+const socket = openSocket('https://cards-game-server.herokuapp.com:8000');
 
 class Games {
     maxPoints = 0;
