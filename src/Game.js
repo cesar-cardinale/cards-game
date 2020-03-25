c5e1c6d54ce4901ba20036dbe31a4401fda0de07
Cardinale <Csar>
2020-03-25 20:25:08 +0100

    Début du plateau de la contrée : choix du mode d'attribution des équipes (2)

--- src/Game.js
+++ src/Game.js
@@ -1,10 +1,9 @@
 import openSocket from 'socket.io-client';
-let socket = null;
+/*let socket = null;
 if(window.location.hostname === 'cards-game-server.herokuapp.com'){
     socket = openSocket('http://cards-game-server.herokuapp.com/:8000');
-} else {
-    socket = openSocket('http://192.168.1.11:8000');
-}
+} else { */
+let socket = openSocket('http://localhost:8000');
 
 class Game {
     maxPoints;
