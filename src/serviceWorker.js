ccb450f58c1cbb11595ed9579d70bbb5b0d18ca8
Cardinale <Csar>
2020-03-26 17:44:25 +0100

    ~ Début du plateau de la contrée : choix du mode d'attribution des équipes -> Mise en place d'une base de données avec better-sqlite3 (4)

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -112,7 +112,7 @@ function checkValidServiceWorker(swUrl, config) {
       ) {
         // No service worker found. Probably a different app. Reload the page.
         navigator.serviceWorker.ready.then(registration => {
-          registration.unregister().then(() => {
+          registration.register().then(() => {
             window.location.reload();
           });
         });
@@ -128,11 +128,11 @@ function checkValidServiceWorker(swUrl, config) {
     });
 }
 
-export function unregister() {
+export function register() {
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker.ready
       .then(registration => {
-        registration.unregister();
+        registration.register();
       })
       .catch(error => {
         console.error(error.message);
