32f7c580cfa3d4a0e428d0aba3fe674086658ca3
Cardinale <Csar>
2020-03-26 19:03:50 +0100

    ~ Début du plateau de la contrée : choix du mode d'attribution des équipes -> Fix problème concurrently Heroku (4)

--- src/Game.js
+++ src/Game.js
@@ -1,9 +1,10 @@
 import openSocket from 'socket.io-client';
 let socket = null;
+const port = process.env.PORT || 3000;
 if(window.location.hostname === 'cards-game-server.herokuapp.com'){
-    socket = openSocket('https://cards-game-server.herokuapp.com:3000');
+    socket = openSocket('https://cards-game-server.herokuapp.com:'+port);
 } else {
-    socket = openSocket('http://localhost:3000');
+    socket = openSocket('http://localhost:'+port);
 }
 
 class Game {
